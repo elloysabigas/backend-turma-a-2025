4ac0ba9700f92a464d84566123479a6f9b3ec1a5
elloysabigas <161051191+elloysabigas@users.noreply.github.com>
2025-02-25 01:36:44 +0000

    update get delete

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,16 +1,37 @@
 const PaymentController = {
 
-   async createPayment(req, res){
-
-    try {
-        res.status(201).json({ message: 'Payment created successfully' });
-    } catch (error) {
-        res.status(500).json({ message: error. message });
+    async createPayment(req, res){
+        try {
+            res.status(201).json({ message: 'Payment created successfully' });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+ 
+    async updatePayment(req, res){
+        try {
+            res.status(200).json({ message: 'Payment updated successfully' });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+ 
+    async deletePayment(req, res){
+        try {
+            res.status(200).json({ message: 'Payment deleted successfully' });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+ 
+    async getPayment(req, res){
+        try {
+            res.status(200).json({ message: 'Payment retrieved successfully' });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
     }
-
-
-   }
-
-}
-
-export default PaymentController;
+ }
+ 
+ export default PaymentController;
+ 
